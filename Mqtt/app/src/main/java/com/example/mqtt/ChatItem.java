24a0038c0fc5b3eca1593f83a4a7188b208cc31f
rnjsdhwp <rnjsdhwp12@naver.com>
2021-06-06 15:47:27 +0900

    ras

---
+++ Mqtt/app/src/main/java/com/example/mqtt/ChatItem.java
@@ -0,0 +1,27 @@
+package com.example.mqtt;
+
+public class ChatItem {
+    private String id;
+    private String content;
+
+    public ChatItem(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
